ea9c5d0a206a43d198af06dd5f692e06f77420b2
Matt Thorning <matt.thorning@headforwards.com>
2017-10-11 16:00:06 +0100

    models, collections and main app logic programamed

---
+++ js/views/app.js
@@ -0,0 +1,30 @@
+
+var app = app || {};
+
+app.AppView = Backbone.View.extend({
+
+	el: '#todoapp',
+
+	statsTemplate: _.template($('#stats_template').html()),
+
+	initialize: function() {
+		this.allCheckbox = this.$('#toggle-all')[0];
+		this.$input = this.$('#new-todo');
+		this.$footer = this.$('#footer');
+		this.$main = this.$('#main');
+
+		this.listenTo(app.Todos, 'add', this.addOne);
+		this.listenTo(app.Todos, 'reset', this.addAll);
+	},
+
+	addOne: function(todo) {
+		var view = new app.TodoView({ model: todo });
+		$('#todolist').append(view.render().el);
+	},
+
+	addAll: function() {
+		this.$('#todo-list').html('');
+		app.Todos.each(this.addOne, this);
+	}
+
+});
